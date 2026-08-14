22e0fecf4d5dcd81c9457504f4ca90b36820ef0a
Shreyash <shreyashkatiyar2020@gmail.com>
2026-08-14 10:45:37 +0530

    Time: 1 ms (99.22%) | Memory: 47.6 MB (9.70%) - LeetSync

---
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int n= nums.length;
+        int []ans = new int[2*n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            ans[ans.length-i-1]=nums[i];
+        }return ans;
+    }
+}
